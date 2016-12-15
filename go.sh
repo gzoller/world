18b4b55765c71bb74d99fcd959c6373af6f6ddfd
Greg Zoller <gwzoller@yahoo.com>
2016-12-15 00:12:22 -0600

    remove test label

--- go.sh
+++ go.sh
@@ -9,4 +9,4 @@ HOST_IP=`ifconfig en0 | grep inet | grep -v inet6 | awk '{print $2}'`
 
 # TODO: If HOST_IP not passed in, assume AWS and hit AWS's host-getter URL
 #
-docker run -it -P -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$HOST_IP $extra gzoller/world2
+docker run -it -P -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$HOST_IP $extra gzoller/world
