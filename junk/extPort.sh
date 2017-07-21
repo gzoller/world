bc2dc7e7f9fdb9f590a133478bb04fe1f9f17776
gzoller <gzoller@gmail.com>
2017-07-21 14:45:21 -0500

    Fix eventstore ip and port

--- junk/extPort.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Get port mappings and parse output into inside outside ports
-IID=`hostname`
-EXTERNALPORT=`docker port $IID | sed 's/\([0-9]*\).*:\([0-9]*\)/\1 \2/' | grep $1 | sed 's/^[0-9]* \([0-9]*\)/\1/'`
-echo $EXTERNALPORT
